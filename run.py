667cae2c8ee83462f89e8bb6b52ea3dab3b11c39
alejandroalemanaleman <alemanaleman.alejandro@gmail.com>
2023-10-22 21:26:29 +0100

    second commit

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ import search
 ab = search.GPSProblem('A', 'B'
                        , search.romania)
 
+#   Anchura-amplitud
 #print(search.breadth_first_graph_search(ab).path())
+#   Profundidad
 #print(search.depth_first_graph_search(ab).path())
+
+#   Ramificación y acotación
 print(search.branch_and_bound(ab).path())
 lista = [1,9123,3,4]
 lista= sorted(lista, key=lambda node: node)
